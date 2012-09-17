fde4f147b8e41d6c7d518d2a24db59361f2f23f5
Harry Nystad <harry.ny@live.no>
2012-09-17 13:10:22 +0200

    begining of initiation at character and draw fuction for that class

---
+++ source/classes/Character.cpp
@@ -0,0 +1,27 @@
+#include "Character.hpp"
+#include<SFML\Graphics.hpp>
+#include <SFML\Graphics\Image.hpp>
+#include <string.h>
+
+Character::Character()
+{
+	
+	// Declare and load a texture
+	sf::Texture texture;
+	texture.loadFromFile("img/player");
+
+	// Create a sprite
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+	sprite.setTextureRect(sf::IntRect(10, 10, 50, 30));
+	sprite.setColor(sf::Color(255, 255, 255, 200));
+	sprite.setPosition(100, 25);
+
+	posX=0;
+	posY=0;
+}
+
+void Character::draw()
+{
+
+}
---
+++ source/classes/Character.hpp
@@ -0,0 +1,12 @@
+class Character
+{
+private:
+	int posX;
+	int posY;
+protected:
+
+public:
+
+	Character();
+	void draw();
+}
