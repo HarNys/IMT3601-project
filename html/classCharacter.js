818406aa300c696536767dc0785c4cd9cf1c6da4
Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
2012-09-23 16:46:35 +0200

    added a bit to world, has segfault at Tile::Tile():isWall = wall

--- html/classCharacter.js
+++ html/classCharacter.js
@@ -1,10 +1,7 @@
 var classCharacter =
 [
     [ "Character", "classCharacter.html#adc27bdd255876169bad2ed0bae0cffb5", null ],
-    [ "characterMovementDown", "classCharacter.html#abdbc133e2f04e8e905d16627f06b0199", null ],
-    [ "characterMovementLeft", "classCharacter.html#a8ced6ea22cd31b916b5d93519e16d96b", null ],
-    [ "characterMovementRight", "classCharacter.html#aa3d311da439b60642b6ada277e217dc9", null ],
-    [ "characterMovementUp", "classCharacter.html#abdfcf4810b53b632e83b9643f4b3f763", null ],
+    [ "characterMovement", "classCharacter.html#ae2c6d71f358ff6ea3623d92cb5c5bfbb", null ],
     [ "draw", "classCharacter.html#af69b0dbd3e55f7fa4a0776063cee0aa0", null ],
     [ "draw", "classCharacter.html#a45a63f9c964bc8286f691fa3eec7ad77", null ],
     [ "updatePosition", "classCharacter.html#ab540f21c0b2bf2d4604d67fa8cad8d0a", null ]
---
+++ html/search/all_70.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['placecharacter',['placeCharacter',['../classWorld.html#a857b7a69c96fa3b0989cf7f20dd4bb8b',1,'World']]]
+];
