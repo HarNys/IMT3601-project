5f9e13c69342ce185dbedcc1601d9fcc8ad14387
Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
2012-12-06 01:50:40 +0100

    Problem when writing to file
    Trying to figure out a good way of writing to file
    	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,15 @@ int main(int argc, char **argv)
 		bool fullscreen; ///< fullscreen the application if true
 	} confSettings;
 	// Reading configuration file, if there is one
+	bool emptyfile = false; ///< true if confiFile is empty
 	//std::fstream configFile;
 	FILE *configFile;
 	configFile = fopen("game.ini", "r");//std::fstream::in | std::fstream::out);
 	if (configFile == NULL)
 	{
 		//printf("main(int,char**): Could not open configuration file 'game.ini' \n");
-		perror("main(int,char**): Could not open configuration file 'game.ini' \n");
+		perror("main(int,char**): Could not open configuration file 'game.ini'");
+		emptyfile = true;
 	}
 	else
 	{
@@ -37,25 +39,26 @@ int main(int argc, char **argv)
 			printf("main(int,char**): Configuration file 'game.ini' contains: \n");
 			// set variables
 		}
-		else
-		{
-			printf("main(int,char**): Writing default configuration file 'game.ini' \n");
-			//std::string printConf = "# Configuration file for Frank Darkhawk's Amazing Maze rpg\n";
-			fprintf(configFile,"# Configuration file for Frank Darkhawk's Amazing Maze rpg\n");
-			//char buffer[256];
-			// generate file from defaults
-			confSettings.screenwidth = 800;
-			fprintf(configFile,"screenwidth %d\n", confSettings.screenwidth);
-			//printConf.append(buffer);
-			confSettings.screenheight = 600;
-			fprintf(configFile,"screenheight %d\n", confSettings.screenheight);
-			//printConf.append(buffer);
-			confSettings.fullscreen = false;
-			fprintf(configFile,"fullscreen %d\n", confSettings.fullscreen);
-			//printConf.append(buffer);
-			//printf("%s",printConf.c_str());
-			//fprintf(configFile,"%s",printConf.c_str());
-		}
+	}
+	if (emptyfile)
+	{
+		printf("main(int,char**): Writing default configuration file 'game.ini' \n");
+		std::string printConf = "# Configuration file for Frank Darkhawk's Amazing Maze rpg\n";
+		//fprintf(configFile,"# Configuration file for Frank Darkhawk's Amazing Maze rpg\n");
+		char buffer[256];
+		// generate file from defaults
+		confSettings.screenwidth = 800;
+		sprintf(buffer,"screenwidth %d\n", confSettings.screenwidth);
+		printConf.append(buffer);
+		confSettings.screenheight = 600;
+		sprintf(buffer,"screenheight %d\n", confSettings.screenheight);
+		printConf.append(buffer);
+		confSettings.fullscreen = false;
+		sprintf(buffer,"fullscreen %d\n", confSettings.fullscreen);
+		printConf.append(buffer);
+		printf("%s",printConf.c_str());
+		freopen(NULL, "w", configFile);
+		fprintf(configFile,"%s",printConf.c_str());
 	}
 
 	return 0;
