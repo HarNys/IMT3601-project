5ec452d68ab49d6b8db5cb55ec98bcc799428a1e
Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
2012-12-07 12:48:02 +0100

    Added copyright notice to all files
    	modified:   src/Includes.hpp
    	modified:   src/classes/Character.cpp
    	modified:   src/classes/Character.hpp
    	modified:   src/classes/CharacterFactory.cpp
    	modified:   src/classes/CharacterFactory.hpp
    	modified:   src/classes/Menu.cpp
    	modified:   src/classes/Menu.hpp
    	modified:   src/classes/Mine.cpp
    	modified:   src/classes/Mine.hpp
    	modified:   src/classes/MineFactory.cpp
    	modified:   src/classes/MineFactory.hpp
    	modified:   src/classes/Node.cpp
    	modified:   src/classes/Node.hpp
    	modified:   src/classes/NonPlayerCharacter.cpp
    	modified:   src/classes/NonPlayerCharacter.hpp
    	modified:   src/classes/Player.cpp
    	modified:   src/classes/Player.hpp
    	modified:   src/classes/StackNode.cpp
    	modified:   src/classes/StackNode.hpp
    	modified:   src/classes/Tile.cpp
    	modified:   src/classes/Tile.hpp
    	modified:   src/classes/World.cpp
    	modified:   src/classes/World.hpp
    	modified:   src/main.cpp

--- src/classes/Menu.hpp
+++ src/classes/Menu.hpp
@@ -1,3 +1,26 @@
+/*
+ * Menu.hpp
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
 #ifndef __MENU__HEADER__GUARD__
 #define __MENU__HEADER__GUARD__
 
@@ -22,7 +45,7 @@ private:
 	int imageCountY;
 	bool gameRuning;
 	sf::Clock timer;
-	
+
 public:
 	Menu(sf::RenderWindow* renderWindow);
 	void mainDraw();
--- src/classes/Node.hpp
+++ src/classes/Node.hpp
@@ -1,3 +1,25 @@
+/*
+ * Node.hpp
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
 #ifndef __NODE__HEADER__GUARD__
 #define __NODE__HEADER__GUARD__
 #include "../Includes.hpp"
@@ -17,16 +39,16 @@ private:
 	Node* rightChild;
 	Node* upChild;
 	Node* downChild;
-	
+
 protected:
 
 public:
-	
+
 	Node(int posx, int posy, int step, int goalXValue, int goalYValue, Node* par = NULL);
 	int getXPos ();
 	int getYPos ();
 	float getPriority();
-	int getLevel(); 
+	int getLevel();
 	void setVisit();
 	void unVisit();
 	bool checkVisited();
@@ -39,7 +61,7 @@ public:
 	bool setRightChild(Node* newChild);
 	bool setDownChild(Node* newChild);
 	bool setLeftChild(Node* newChild);
-	
+
 	Node* findCheapestUnusedRecursively();
 	bool checkTreeRecursivelyForNode(int xCoordinates, int yCoordinates);
 	~Node();
