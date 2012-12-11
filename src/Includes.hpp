eee6e10c3191c18172f2cbee2eaa76fe4ef6d706
ørjan røkkum brandtzæg <orod_sul@hotmail.com>
2012-12-11 14:05:52 +0100

    added includes for windows

--- src/Includes.hpp
+++ src/Includes.hpp
@@ -30,7 +30,8 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <SFML/Audio/Music.hpp>
-#include <SFML/System/Thread.hpp>
+#include <SFML/Network.hpp>
+#include <pthread.h>
 
 #elif defined __linux__
 //we are on linux
