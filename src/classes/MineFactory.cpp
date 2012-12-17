d8475b7a940fd0ef28374c379199f6a629275b93
Thomas Sigurdsen <thomas.sigurdsen@gmail.com>
2012-12-17 10:16:16 +0100

    Fix wrong email address
    	modified:   src/Includes.hpp
    	modified:   src/classes/Character.cpp
    	modified:   src/classes/Character.hpp
    	modified:   src/classes/CharacterFactory.cpp
    	modified:   src/classes/CharacterFactory.hpp
    	modified:   src/classes/Mine.hpp
    	modified:   src/classes/MineFactory.cpp
    	modified:   src/classes/MineFactory.hpp
    	modified:   src/classes/Tile.cpp
    	modified:   src/classes/Tile.hpp
    	modified:   src/classes/World.cpp
    	modified:   src/classes/World.hpp
    	modified:   src/main.cpp
    	modified:   src/makefile

--- src/classes/MineFactory.cpp
+++ src/classes/MineFactory.cpp
@@ -1,7 +1,7 @@
 /*
  * MineFactory.cpp
  *
- * Copyright 2012 Thomas Sigurdsen <thomas@gmail.com>
+ * Copyright 2012 Thomas Sigurdsen <thoams.sigurdsen@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
